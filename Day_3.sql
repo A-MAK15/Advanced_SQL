ff8a36f35c36bd6149657ca48a7edadb69fc480c
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2026-01-02 22:02:30 +0200

    Update Day_3.sql

--- Day_3.sql
+++ Day_3.sql
@@ -1,1 +1,14 @@
-
+-- NTILE
+SELECT * FROM (
+	WITH base_table AS (
+		SELECT customer_id, SUM(units * unit_price) AS total_spend FROM orders ord
+		LEFT JOIN products pd
+		ON ord.product_id = pd.product_id
+		-- ORDER BY 1
+		GROUP BY 1
+	)
+	SELECT *,
+		NTILE(100) OVER(PARTITION BY customer_id ORDER BY total_spend) AS spend_pct
+	FROM base_table
+) AS spend_pct
+WHERE spend_pct = 1;
