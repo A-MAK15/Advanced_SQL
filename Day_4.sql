39568be9b7447f297dbdca73e080fcf372701573
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2026-01-03 22:31:28 +0200

    Update Day_4.sql

--- Day_4.sql
+++ Day_4.sql
@@ -1,1 +1,4 @@
-
+-- Moving Averages
+SELECT country, year, happiness_score, 
+	   AVG(happiness_score) OVER(PARTITION BY country ORDER BY year ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS three_year_ma
+FROM happiness_scores;
