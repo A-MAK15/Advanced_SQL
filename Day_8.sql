63cf5e45b8d486da75e1b0b28b831ace3e247b7e
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2026-01-09 21:29:12 +0200

    Update Day_8.sql

--- Day_8.sql
+++ Day_8.sql
@@ -1,1 +1,8 @@
-
+-- Pattern Matching
+-- INSTR and SUBTSR
+SELECT event_name,
+	   CASE WHEN INSTR(event_name, ' ') = 0 THEN event_name
+	   ELSE SUBSTR(event_name, 1, INSTR(event_name, ' ') - 1)
+      -- INSTR(event_name, ' ')
+       END AS first_word
+FROM my_events;
