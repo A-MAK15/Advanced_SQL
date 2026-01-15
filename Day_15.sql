60839adbcd37c0123c6aca2075ae4fe36f238a6e
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2026-01-15 22:07:39 +0200

    Update Day_15.sql

--- Day_15.sql
+++ Day_15.sql
@@ -1,1 +1,13 @@
+-- Final Projects
+-- New tables players, salaries, schools, school_details;
+SELECT * FROM players;
 
+SELECT * FROM schools;
+
+SELECT * FROM school_details;
+
+SELECT * FROM salaries;
+
+SELECT DISTINCT(yearID) FROM schools s
+LEFT JOIN  school_details sd
+ON s.playerID = s.playerID
