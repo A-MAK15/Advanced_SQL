77352a2eea79c32f679327233bb64bfe72f00711
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2026-01-17 22:46:10 +0200

    Update Day_17.sql

--- Day_17.sql
+++ Day_17.sql
@@ -1,1 +1,11 @@
-
+-- 2.
+WITH top_five AS(
+	SELECT schoolID, COUNT(*) AS top_schools
+	FROM schools
+	GROUP BY 1
+)
+SELECT * FROM top_five tf
+LEFT JOIN school_details sd 
+ON tf.schoolID = sd.schoolID
+ORDER BY top_schools DESC
+LIMIT 5
