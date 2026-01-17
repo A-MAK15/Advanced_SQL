c4d9ccc04c7649caa050f56283c3917f45afb468
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2026-01-17 22:45:51 +0200

    Update Day_16.sql

--- Day_16.sql
+++ Day_16.sql
@@ -1,1 +1,5 @@
-
+SELECT yearID,
+	   COUNT(playerID) OVER(ORDER BY yearID ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS num_of_players
+FROM schools s
+LEFT JOIN  school_details sd
+ON s.playerID = s.playerID;
