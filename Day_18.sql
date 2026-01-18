a812a4123d312c6754ad335e2c2ba337a2aa17e0
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2026-01-18 20:11:39 +0200

    Update Day_18.sql

--- Day_18.sql
+++ Day_18.sql
@@ -1,1 +1,12 @@
-
+SELECT *,
+	SUM(decade_rn) OVER(ORDER BY yearID ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS decade_sum
+FROM (
+	WITH players AS (
+		SELECT DISTINCT playerID, schoolID, yearID,
+			ROW_NUMBER() OVER(PARTITION BY playerID ORDER BY yearID) AS RN
+		FROM schools
+	)
+	SELECT *,
+		ROW_NUMBER() OVER(ORDER BY yearID) AS decade_rn
+	FROM players
+) AS players_rn
