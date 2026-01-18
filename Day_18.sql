5aee7a033b35244e5d88706fb5c0335ca47f7b1d
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2026-01-18 20:13:06 +0200

    Update Day_18.sql

--- Day_18.sql
+++ Day_18.sql
@@ -1,3 +1,4 @@
+-- 1.
 SELECT *,
 	SUM(decade_rn) OVER(ORDER BY yearID ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS decade_sum
 FROM (
@@ -9,4 +10,29 @@ FROM (
 	SELECT *,
 		ROW_NUMBER() OVER(ORDER BY yearID) AS decade_rn
 	FROM players
-) AS players_rn
+) AS players_rn;
+
+-- 2.
+WITH top_five AS(
+	SELECT schoolID,COUNT(*) AS top_schools
+	FROM schools
+	GROUP BY 1
+)
+SELECT name_full FROM top_five tf
+LEFT JOIN school_details sd 
+ON tf.schoolID = sd.schoolID
+ORDER BY top_schools DESC
+LIMIT 5;
+
+-- 3. 
+WITH top_five AS(
+	SELECT schoolID, yearID ,COUNT(*) AS top_schools
+	FROM schools
+	GROUP BY 1, 2
+)
+SELECT *,
+	   ROW_NUMBER() OVER(ORDER BY top_schools DESC ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS RN
+FROM top_five tf
+LEFT JOIN school_details sd 
+ON tf.schoolID = sd.schoolID
+ORDER By yearID 
