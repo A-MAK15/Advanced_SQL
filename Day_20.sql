f1c082eef30aeb7703154ae9f184cc2a6f3f8cb2
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2026-01-20 22:26:27 +0200

    Update Day_20.sql

--- Day_20.sql
+++ Day_20.sql
@@ -1,1 +1,12 @@
+-- 1.
+SELECT COUNT(*) * 0.2 FROM (
+	WITH top_20_per AS (
+		SELECT *,
+			AVG(salary) OVER(PARTITION BY teamID ORDER BY yearID) AS average_salary
+		FROM salaries
+	)
+	SELECT DISTINCT yearID, teamID, average_salary FROM top_20_per
+	ORDER BY average_salary DESC
+) AS percent_20
+LIMIT (SELECT COUNT(*) * 0.2 FROM percent_20);
 
