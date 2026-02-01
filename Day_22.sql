57eacbb662856629e8aa8a9b288dd2e3874b4acb
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2026-02-01 21:25:15 +0200

    Update Day_22.sql

--- Day_22.sql
+++ Day_22.sql
@@ -11,3 +11,33 @@ FROM (
 	SELECT *, CAST(birthDate AS DATE) AS birthDate_converted
 	FROM to_date
 ) AS date_concate;
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+--b)
+SELECT *, TIMESTAMPDIFF(YEAR, birthDate_converted, debut) AS years_debut ,TIMESTAMPDIFF(YEAR, birthDate_converted, finalGame) AS years_final_game
+FROM (
+	WITH to_date AS (
+		SELECT *, CONCAT(birthYear, '-', birthMonth, '-', birthDay) AS birthDate
+		FROM players
+	)
+	SELECT *, CAST(birthDate AS DATE) AS birthDate_converted
+	FROM to_date
+) AS date_concate;
+
+
+-- c)
+WITH career AS (
+	SELECT *, TIMESTAMPDIFF(YEAR, birthDate_converted, debut) AS years_debut ,TIMESTAMPDIFF(YEAR, birthDate_converted, finalGame) AS years_final_game
+	FROM (
+		WITH to_date AS (
+			SELECT *, CONCAT(birthYear, '-', birthMonth, '-', birthDay) AS birthDate
+			FROM players
+		)
+		SELECT *, CAST(birthDate AS DATE) AS birthDate_converted
+		FROM to_date
+	) AS date_concate
+)
+SELECT *, years_final_game - years_debut AS career_length
+FROM career
+ORDER BY years_final_game - years_debut DESC
+
