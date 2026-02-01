3ee9a8296b250e7b148850e10a650489016e67af
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2026-02-01 21:24:26 +0200

    Update Day_22.sql

--- Day_22.sql
+++ Day_22.sql
@@ -1,4 +1,13 @@
 -- Player Analysis
 SELECT * FROM players;
 
--- Day 2
+-- a) Age Debut
+SELECT *, TIMESTAMPDIFF(YEAR, birthDate_converted, debut) AS years
+FROM (
+	WITH to_date AS (
+		SELECT *, CONCAT(birthYear, '-', birthMonth, '-', birthDay) AS birthDate
+		FROM players
+	)
+	SELECT *, CAST(birthDate AS DATE) AS birthDate_converted
+	FROM to_date
+) AS date_concate;
