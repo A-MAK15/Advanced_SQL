12e754412ba2baaa378a4ec626bd9d4bf2940d13
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2026-02-02 22:58:25 +0200

    Update Day_22.sql

--- Day_22.sql
+++ Day_22.sql
@@ -41,3 +41,19 @@ SELECT *, years_final_game - years_debut AS career_length
 FROM career
 ORDER BY years_final_game - years_debut DESC
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- 2.a)
+-- Team on starting and ending
+-- WITH base_starting AS ;
+WITH team_start AS (
+	SELECT * FROM (
+		SELECT p.playerID, nameGiven, debut, teamID AS team_start,
+			ROW_NUMBER() OVER(PARTITION BY p.playerID ORDER BY debut ASC) AS RN
+		FROM players p
+		LEFT JOIN salaries s
+		ON p.playerID = s.playerID
+	) AS team_started_with
+)
+SELECT *
+FROM team_start
+WHERE RN = 1;
